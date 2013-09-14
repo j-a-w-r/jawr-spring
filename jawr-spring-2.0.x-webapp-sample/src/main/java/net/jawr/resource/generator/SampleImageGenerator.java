a7e3c1ea3a1550f6e9bd9ce8a51ce1292975f212
Ibrahim Chaehoi <ichaehoi@gmail.com>
2013-09-14 22:53:30 +0000

    spring-2.0.x clean up
    git-svn-id: https://svn.java.net/svn/jawr~svn/extensions/spring-2.0.x/trunk@871 4db09855-0bbe-6bc9-8408-d5225a1fabd9

--- jawr-spring-2.0.x-webapp-sample/src/main/java/net/jawr/resource/generator/SampleImageGenerator.java
+++ jawr-spring-2.0.x-webapp-sample/src/main/java/net/jawr/resource/generator/SampleImageGenerator.java
@@ -5,7 +5,10 @@ import java.io.InputStream;
 import javax.servlet.ServletContext;
 
 import net.jawr.web.resource.bundle.generator.GeneratorContext;
+import net.jawr.web.resource.bundle.generator.ResourceGenerator;
 import net.jawr.web.resource.bundle.generator.StreamResourceGenerator;
+import net.jawr.web.resource.bundle.generator.resolver.PrefixedPathResourceGeneratorResolver;
+import net.jawr.web.resource.bundle.generator.resolver.ResourceGeneratorResolver;
 
 public class SampleImageGenerator implements StreamResourceGenerator {
 
@@ -19,10 +22,16 @@ public class SampleImageGenerator implements StreamResourceGenerator {
 	}
 
 	/* (non-Javadoc)
-	 * @see net.jawr.web.resource.bundle.generator.PrefixedResourceGenerator#getMappingPrefix()
+	 * @see net.jawr.web.resource.bundle.generator.BaseResourceGenerator#getResolver()
 	 */
-	public String getMappingPrefix() {
-		return "img";
+	@Override
+	public ResourceGeneratorResolver getResolver() {
+		return new PrefixedPathResourceGeneratorResolver("img");
+	}
+
+	@Override
+	public String getDebugModeRequestPath() {
+		return ResourceGenerator.IMG_DEBUGPATH;
 	}
 
 }
